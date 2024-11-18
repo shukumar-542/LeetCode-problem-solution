f445b5346df686fce1d3a7eca408d98afb78fa51
shukumar-542 <shukumar542@gmail.com>
2024-11-18 23:16:44 +0600

    Solved subsequence problem

---
+++ isSubsequence.js
@@ -0,0 +1,10 @@
+var isSubsequence = function (s, t) {
+    let index = 0 ; 
+    for(let i = 0 ;  i < t.length ; i++){
+        if(index < s.length && s[i] === t[i]){
+            index++
+        }
+    }
+    return index.length === s.length
+};
+console.log(isSubsequence("acb", "ahbgdc"))
