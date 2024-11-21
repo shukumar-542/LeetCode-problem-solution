b81c6efab2c8a119b87a094ff6fb9362e94eaff5
shukumar-542 <shukumar542@gmail.com>
2024-11-21 15:18:48 +0600

    Solved majority element problem

---
+++ majorityElement.js
@@ -0,0 +1,18 @@
+var majorityElement = function(nums) {
+    let maxValue = 0
+    let key = 0
+    const allNumbers = {}
+    nums.forEach(num =>{
+        allNumbers[num] = (allNumbers[num] || 0) + 1
+    }) 
+    for(large in allNumbers){
+        if(allNumbers[large] > maxValue){
+            maxValue = allNumbers[large]
+            key = large
+        }
+
+    }
+    console.log(key);
+};
+
+majorityElement([3,3,4])
