467440aef591c150872d528fdc6ccf969eab20b3
shukumar-542 <shukumar542@gmail.com>
2024-11-29 21:37:20 +0600

    Solve remove duplicate problem

---
+++ removeDuplicate.js
@@ -0,0 +1,15 @@
+var removeDuplicates = function(nums) {
+    let index = 0
+    for(let i = 0 ; i < nums.length ; i++){
+        
+        if(i === 0 || nums[i] !== nums[i - 1]){
+            nums[index] = nums[i]
+            index ++
+        }
+
+
+    }
+    return index
+};
+const result = removeDuplicates([1,1,2 , 2,3])
+console.log(result);
