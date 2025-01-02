503e1d98e15700c77e4327889428563052da67e4
shukumar-542 <shukumar542@gmail.com>
2025-01-02 18:02:07 +0600

    Structured merge array problem

---
+++ findTheIndex.js
@@ -0,0 +1,18 @@
+var strStr = function(haystack, needle) {
+    // console.log(haystack);
+    // console.log(needle.length);
+    let index = 0
+    for(let i = 0 ; i<haystack.length ; i++){
+        for(let j = 0 ; j<needle.length ; j++){
+            console.log(needle[j]);
+            if(needle[j] === haystack[i]){
+                
+            }
+        }
+    }
+
+    
+};
+strStr("sadbutsad" ,"sad")
+"leeto"
+"leetcode"
---
+++ merge.js
@@ -0,0 +1,9 @@
+
+const nums1 = [1,2,3,0,0,0]
+const m = 3
+const  nums2 = [2,5,6]
+const n = 3
+var merge = function(nums1, m, nums2, n) {
+    
+};
+merge(nums1, m, nums2, n)
