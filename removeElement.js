d1b3477fe2d5027cb7d48cbad65adbc10bb4fcf1
shukumar-542 <shukumar542@gmail.com>
2025-03-12 14:12:58 +0600

    Solve remove element problem

--- removeElement.js
+++ removeElement.js
@@ -7,10 +7,10 @@ var removeElement = function(nums, val) {
     let k = 0;
     for(let i  = 0 ; i<nums.length ; i++ ){
         if(nums[i] !== val){
+            nums[k] = nums[i]
             k += 1;
         }
     }
-    // console.log(k);
     return k;
     
 };
