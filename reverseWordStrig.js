fadea8bfc5a0b959973ddfb29e34455e3a240c8b
shukumar-542 <shukumar542@gmail.com>
2025-03-12 17:42:17 +0600

    Solve reverse word string

---
+++ reverseWordStrig.js
@@ -0,0 +1,8 @@
+s ="the sky is blue"
+
+var reverseWords = function(s) {
+    return s.split(" ").filter(srt => srt !== "").reverse().join(" ")
+   
+};
+
+console.log(s.split(" ").filter(srt => srt !== "").reverse().join(" "));
