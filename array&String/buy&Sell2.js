1350ad1d17da3f264bac6fa3b60ae329f75ce395
shukumar-542 <shukumar542@gmail.com>
2025-09-03 12:39:46 +0600

    Solve max profit buy and sell problem

---
+++ array&String/buy&Sell2.js
@@ -0,0 +1,14 @@
+var maxProfit = function(prices) {
+    console.log(prices)
+    let profit = 0
+    for(let i = 1; i < prices.length; i++) {
+        if(prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1]
+        }
+    }
+    return profit
+};
+
+
+const profit = maxProfit([7,1,5,3,6,4]) // 5
+console.log(profit)
