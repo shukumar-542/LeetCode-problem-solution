c21ff540bae14102229210d58ead6280c284d288
shukumar-542 <shukumar542@gmail.com>
2025-10-15 16:27:21 +0600

    Solve contains duplicate leetcode problems

--- hashMap/containsDuplicate.js
+++ hashMap/containsDuplicate.js
@@ -1,5 +1,24 @@
-var containsNearbyDuplicate = function(nums, k) {
-    
+var containsNearbyDuplicate = function (nums, k) {
+    const nMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const element = nums[i];
+
+        if (nMap.has(element)) {
+            const prevIndex = nMap.get(element);
+            const diff = i - prevIndex;
+
+            if (diff <= k) {
+                return true;
+            }
+        }
+
+        nMap.set(element, i);
+    }
+
+    return false;
+
 };
 
-containsNearbyDuplicate([1,2,3,1] , 3)
+const result = containsNearbyDuplicate([1, 0, 1, 1], 1);
+console.log(result)
