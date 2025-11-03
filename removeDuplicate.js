f8a1f2bccfdf6fad56b89bed54bd4f4235a14d06
shukumar-542 <shukumar542@gmail.com>
2025-11-03 12:53:58 +0600

    Fix remove duplicate leet code problem issue

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,17 +1,20 @@
 var removeDuplicates = function(nums) {
     let index = 0
+    let newArr  = []
     for(let i = 0 ; i < nums.length ; i++){
         
         if(i === 0 || nums[i] !== nums[i - 1]){
             nums[index] = nums[i]
             index ++
+            newArr.push(nums[i])
         }
 
 
 
     }
+    console.log(newArr)
     return index
 };
 
-const result = removeDuplicates([1,1,2,3])
+const result = removeDuplicates([1,1,2,3,3])
 console.log(result);
