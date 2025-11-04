18b1626e53dcbc3b95ab6c63b6756a037fee3a15
shukumar-542 <shukumar542@gmail.com>
2025-11-04 12:50:17 +0600

    Trying to solve letter combintaions leetcode problem

---
+++ Medium/letterCombinations.js
@@ -0,0 +1,20 @@
+var letterCombinations = function(digits) {
+    let map = {
+        2 : "abc",
+        3 : "def",
+        4 : "ghi",
+        5 : 'jkl',
+        6 : 'mno',
+        7 : "qrs",
+        8 : "tuv", 
+        9 : "wxyz"
+    }
+    if(!digits) return [];
+
+    let result = [];
+    
+
+
+    
+};
+letterCombinations("23")
