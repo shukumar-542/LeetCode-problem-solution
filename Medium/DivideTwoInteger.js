b9b89d307c5f748dc39d648807ab53e46319d457
shukumar-542 <shukumar542@gmail.com>
2025-11-06 12:38:50 +0600

    Solve divide two integer leetcode problem

---
+++ Medium/DivideTwoInteger.js
@@ -0,0 +1,16 @@
+var divide = function(dividend, divisor) {
+     const INT_MAX = 2147483647;
+    const INT_MIN = -2147483648;
+
+    if (dividend === INT_MIN && divisor === -1) {
+        return INT_MAX;
+    }
+
+
+    const result = dividend / divisor;
+    console.log(result)
+
+    return Math.floor(result)
+    
+};
+divide(-2147483648 , -1)
