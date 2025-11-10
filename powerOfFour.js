60a107b67bc4d73b35bd97c29b1a09dde2c1410e
shukumar-542 <shukumar542@gmail.com>
2025-11-10 10:59:47 +0600

    Solve power of four leetcode problem

---
+++ powerOfFour.js
@@ -0,0 +1,12 @@
+var isPowerOfFour = function(n) {
+
+    if(n <= 0) return false
+
+    while (n % 4 === 0) {
+        n = n / 4
+    }
+
+    return n === 1
+    
+};
+console.log(isPowerOfFour(5))
