397ff21bb14918bd0ac23a892b6348f9c53be776
shukumar-542 <shukumar542@gmail.com>
2025-11-12 17:00:47 +0600

    Solve add string leetcode problem

---
+++ addStrings.js
@@ -0,0 +1,7 @@
+var addStrings = function(num1, num2) {
+    const total = Number(num1) + Number(num2)
+    console.log(total)
+    return total.toString()
+    
+};
+console.log(addStrings("11", "123"))
