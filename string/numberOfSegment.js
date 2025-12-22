4f40ac0232172bd33a76adaa0ff68c5f027dd188
shukumar-542 <shukumar542@gmail.com>
2025-12-22 11:36:14 +0600

    Fix number of segment in string leetcode problem

--- string/numberOfSegment.js
+++ string/numberOfSegment.js
@@ -2,9 +2,9 @@ var countSegments = function(s) {
     if(s.trim() == ""){
         return 0
     }
-    const words = s.trim().split(/\s+/)
+    const words = s.split(" ").filter(word => word !== "")
     console.log(words.length)
     
 };
 
-countSegments("Hello")
+countSegments("Hello   John  ")
